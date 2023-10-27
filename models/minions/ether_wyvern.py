6704810ada645f285987d597ff4459582f1f5a8a
Ludwig2011 <LudwigBlarg@gmail.com>
2023-10-27 13:35:43 +0200

    fix and clean up pyre of wyverns

--- models/minions/ether_wyvern.py
+++ models/minions/ether_wyvern.py
@@ -1,27 +1,23 @@
 from CommonContent import SimpleMeleeAttack, SimpleBurst, TeleportyBuff
 from Level import *
+from mods.EtherealPack.models.buffs.etherealness_buff import EtherealnessBuff
 from mods.EtherealPack.tags.Ethereal import Ethereal 
 from mods.EtherealPack.models.minions.ether_wave import EtherWave
 
-def EtherWyvern(shields=2, elemental=False, ether_link=False):
+def EtherWyvern(shields=2):
 	unit = Unit()
 	unit.name = "Äther Wyvern"
-	unit.description = "tbd"
+	unit.description = "Lets out ether waves"
+	unit.asset_name = os.path.join("..", "..", "mods", "EtherealPack", "ether_hydra")
 	unit.max_hp = 10
-	unit.spells.append(SimpleMeleeAttack(6))
+	unit.spells.append(SimpleMeleeAttack(3))
+	unit.spells.append(EtherWave())
 	unit.shields = shields
 	unit.resists[Ethereal] = 50
 	unit.resists[Tags.Arcane] = -25
 	unit.tags = [Tags.Dragon, Tags.Living, Ethereal]
 	unit.flying = True
 	unit.buffs.append(TeleportyBuff())
-
-	if(ether_link):
-		unit.spells.append(EtherWave())
-	else:
-		burst_spell = SimpleBurst(3, 4, damage_type=Ethereal, cool_down=3, ignore_walls=True, onhit=lambda caster, unit: unit.apply_buff(EtherealnessBuff(), 7))
-		burst_spell.friendly_fire = False
-		unit.spells.append(burst_spell)
 
 
 
--- models/spells/pyre_of_wyverns.py
+++ models/spells/pyre_of_wyverns.py
@@ -1,4 +1,6 @@
+from CommonContent import SimpleMeleeAttack
 from Level import *
+from mods.EtherealPack.models.minions.ether_wave import EtherWave
 from mods.EtherealPack.models.minions.ether_wyvern import EtherWyvern
 from mods.EtherealPack.tags.Ethereal import Ethereal 
 
@@ -7,33 +9,36 @@ class PyreOfWyvernsSpell(Spell):
 	def on_init(self):
 
 		self.name = "Pyre of Wyverns"
+		self.level = 5
 
 		self.minion_health = 10
-		self.minion_damage = 6
-		self.minion_range = 5
+		self.minion_damage = 3
+		self.minion_range = 4
 		self.num_summons = 3
 		self.shields = 2
 
-		self.max_charges = 2
-		self.upgrades['ether_link'] = 1, 4, "Ether Link", "wyverns link up to each other providing shield regeneration if they stay within certain range"
+		self.max_charges = 2		
+		self.upgrades['shields'] = (2, 3)
+		self.upgrades['num_summons'] = (2, 3)
+		self.upgrades['shielding_waves'] = 1, 4, "Shielding Waves", "The wyverns ether waves provide up to 1 shield to allies"
+		self.upgrades['ether_link'] = 1, 5, "Ether Link", "Wyverns link up to each other if they stay near they double their damage and reduce their wave cooldown to 1"
 
 		self.range = 0
-
-		self.level = 5
-		self.tags = [Tags.Dragon, Tags.Living, Ethereal]
+		self.tags = [Tags.Dragon, Tags.Conjuration, Ethereal]
 
 
 
 	def get_description(self):
 		return ("Summons [{num_summons}_wyverns:num_summons] near the caster.\n"
 				"Wyverns have [{minion_health}_HP:minion_health].\n"
+				"Wyverns have a ether wave attack which deals [{minion_damage}_äthereal:äthereal] damage in a [4_tile:radius] radius.\n"
 				"Wyverns have a melee attack which deals [{minion_damage}_physical:physical] damage.").format(**self.fmt_dict())
 
 	def cast_instant(self, x, y):
 		for i in range(self.get_stat('num_summons')):
-
-			wyvern = EtherWyvern(shields=self.get_stat('shields'), ether_link=True if self.get_stat('ether_link') else False)
+			wyvern = EtherWyvern(shields=self.get_stat('shields'))
+			wyvern.spells.clear()
+			wyvern.spells.append(SimpleMeleeAttack(self.get_stat('minion_damage')))
+			wyvern.spells.append(EtherWave(self.get_stat('minion_damage'), self.get_stat('minion_range'), self.get_stat('shielding_waves')))
 			wyvern.team = self.caster.team 
-
-
 			self.summon(wyvern, Point(x, y))
