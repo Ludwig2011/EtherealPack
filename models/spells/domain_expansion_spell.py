8b7f33f82f70f6b3f8b3321fef1b9efe15ec5f3b
Leon Gutknecht-Stöhr <leon.gutknecht-stoehr@outlook.de>
2023-11-05 10:31:15 +0100

    Refined draft

--- models/spells/domain_expansion_spell.py
+++ models/spells/domain_expansion_spell.py
@@ -1,9 +1,6 @@
-from CommonContent import FrozenBuff
 from Level import *
 from mods.EtherealPack.models.hazards.pure_ether import PureEther
 from mods.EtherealPack.tags.Ethereal import Ethereal
-from mods.EtherealPack.models.buffs.etherealness_buff import EtherealnessBuff
-import random
 
 
 class DomainExpansionSpell(Spell):
@@ -17,15 +14,17 @@ class DomainExpansionSpell(Spell):
 
         self.damage = 7
         self.radius = 5
+        self.duration = 4
 
-        self.minion_damage = 0
         self.stun = False
         self.shields = 0
 
+        self.range = 1
 
-              
+
+            
         self.upgrades['stun'] = (True, 25)
-        self.upgrades['minion_damage'] = (1, 25)
+        self.upgrades['damage'] = (1, 25)
         self.upgrades['radius'] = (1, 25)
         self.upgrades['shields'] = (1, 25)
 
@@ -36,23 +35,21 @@ class DomainExpansionSpell(Spell):
     def get_description(self):
         return ("Ooooh much fluff, such storytelling")
 
-    def cast_instant(self):
-
-        # hotile_targets = [u for u in self.caster.level.get_units_in_ball(Point(x, y), self.radius) if u != self.caster and are_hostile(self.caster, u)]
-        # friendly_targets = [u for u in self.caster.level.get_units_in_ball(Point(x, y), self.radius) if u != self.caster and not are_hostile(self.caster, u)]
-
-        # if self.get_stat("stun"):
-        #     for target in hostile_targets:
-        #         #apply stun
+    def cast_instant(self, x, y):
+        hostile_targets = [u for u in self.caster.level.get_units_in_ball(Point(x, y), self.radius) if u != self.caster and are_hostile(self.caster, u)]
+        friendly_targets = [u for u in self.caster.level.get_units_in_ball(Point(x, y), self.radius) if u != self.caster and not are_hostile(self.caster, u)]
+        if self.get_stat("stun"):
+            for target in hostile_targets:
+                target.apply_buff(Stun(), 1)
         
-        # if self.get_stat("stun"):
-        #     for target in friendly_targets:
-        #         #apply shields
-                
+        if self.get_stat("shields"):
+            for target in friendly_targets:
+                if target.shields < 2:
+                    target.shields += 1                
                 
 
         for stage in Burst(self.caster.level, self.caster.x, self.caster.y, self.get_stat('radius')):
-			for point in stage:
-				if self.caster.level.tiles[point.x][point.y].prop == None:
-					pure_ether = PureEther(self.caster,self,self.get_stat('duration'),self.get_stat('minion_damage'))
-					self.caster.level.add_obj(pure_ether, point.x, point.y)
+            for point in stage:
+                if self.caster.level.tiles[point.x][point.y].prop == None:
+                    pure_ether = PureEther(self.caster,self,self.get_stat('duration'),self.get_stat('damage'))
+                    self.caster.level.add_obj(pure_ether, point.x, point.y)
