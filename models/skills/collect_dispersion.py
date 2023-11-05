d6c365708ff0d672dde9f67bbd9ced88aad7ba93
Ludwig2011 <LudwigBlarg@gmail.com>
2023-11-05 10:06:40 +0100

    collect dispersion

--- models/skills/collect_dispersion.py
+++ models/skills/collect_dispersion.py
@@ -1,1 +1,36 @@
 #gain back charges of spells when ätherealised enemies die
+from Level import *
+from mods.EtherealPack.models.buffs.etherealness_buff import EtherealnessBuff
+from mods.EtherealPack.tags.Ethereal import Ethereal
+
+
+class CollectDispersion(Upgrade):
+
+    def on_init(self):
+        self.global_triggers[EventOnDamaged] = self.on_damaged
+        self.name = "Collect Dispersion"
+        self.tags = [Ethereal]
+        self.level = 6
+
+    def on_damaged(self, damage_event):
+
+        # No harvesting your summons
+        if not self.owner.level.are_hostile(self.owner, damage_event.unit):
+            return
+
+        if damage_event.unit.cur_hp <= 0 and damage_event.unit.has_buff(EtherealnessBuff):
+            chance = 0
+            for buff in damage_event.unit.buffs:
+                if isinstance(buff, EtherealnessBuff):
+                    chance = buff.turns_left
+            if damage_event.damage_type == Ethereal:
+                chance *= 3
+            if random.random()*100 < chance:
+                for spell in self.owner.spells:
+                    if Ethereal in spell.tags and spell.cur_charges < spell.get_stat('max_charges'):
+                        spell.cur_charges += 1
+                self.owner.level.show_effect(self.owner.x, self.owner.y, Ethereal)
+
+    def get_description(self):
+        return ("Whenever an enemy unit dies, each of your [äthereal] spells has a chance euqal to 1/100 of the Ätherealness applied to the unit to gain a charge.\n"
+                "This chance is tripled if the unit died to [äthereal] damage.")
--- models/spells/railgun.py
+++ models/spells/railgun.py
@@ -3,3 +3,36 @@
 # max pool of damage that gets reduced per unit hit? to a max per unit?
 # damage in straight line, ?destroy walls based on charge or upgrade?
 # charge speed upgrade
+
+from Level import *
+from mods.EtherealPack.models.buffs.hex_buffs import HexBuff, HexDebuff
+from mods.EtherealPack.tags.Ethereal import Ethereal
+
+class Railgun(Spell):
+	def on_init(self):
+		self.name = "Railgun"
+		self.range = 0 
+		self.tags = [Ethereal, Tags.Metallic, Tags.Scorcery]
+		self.level = 2
+
+		self.damage = 5
+		self.damage_types = [Ethereal, Tags.Physical]
+
+		self.max_charges = 7
+		self.charge_speed = 7
+
+		self.upgrades['charge_speed'] = (1, 3)
+		self.upgrades['deterioration'] = (1, 2, "Deterioration", "[äthereal:äthereal][physical:physical]".format(**self.fmt_dict()))
+		
+
+	def cast(self, x, y):
+		unit = self.caster.level.get_unit_at(x, y)
+		unit.apply_buff(HexDebuff(self.get_stat('resist_loss'), self.get_stat('deterioration')), self.get_stat('duration'))
+		self.caster.apply_buff(HexBuff(self.get_stat('damage'), self.get_stat('volatile_death'), self.get_stat('perpetual_curse'), self.get_stat('resist_loss'), self.get_stat('deterioration'), self.get_stat('duration')), self.get_stat('duration'))
+		yield
+
+	def can_cast(self, x, y):
+		return self.caster.level.get_unit_at(x, y) and Spell.can_cast(self, x, y)
+		
+	def get_description(self):
+		return "Curse enemy to take [{damage}_äthereal:äthereal] and [{damage}_dark:dark] damage whenever they take damage for [{duration}_turns:duration].".format(**self.fmt_dict())
